4d7c840c61e8f604d44ca839d9bd4fc962f66bce
Kim Lindtner <kim.lindtner@skatteetaten.no>
2018-05-08 10:23:59 +0200

    New code to delete projects and apps

--- src/main/kotlin/no/skatteetaten/aurora/gorg/controller/TestController.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gorg/controller/TestController.kt
@@ -1,26 +1,20 @@
 package no.skatteetaten.aurora.gorg.controller
 
 import no.skatteetaten.aurora.gorg.service.CrawlService
-import no.skatteetaten.aurora.model.ProjectInfo
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestController(val crawler: CrawlService) {
 
-    @GetMapping("/api/projectname")
-    fun hello(): List<String> {
-        return crawler.findAllProjects()
+    @GetMapping("/api/delete")
+    fun delete() {
+        crawler.deleteProjects()
     }
 
-    @GetMapping("/api/ttl")
-    fun ttl(): List<ProjectInfo> {
-        return crawler.listTtl()
-    }
-
-    @GetMapping("/api/kim")
-    fun kim() {
-        crawler.testKim()
+    @GetMapping("/api/test")
+    fun test() {
+        crawler.test()
     }
 
 }
--- src/main/kotlin/no/skatteetaten/aurora/gorg/service/CrawlService.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gorg/service/CrawlService.kt
@@ -1,63 +1,116 @@
 package no.skatteetaten.aurora.gorg.service
 
+import io.fabric8.kubernetes.api.model.HasMetadata
+import io.fabric8.openshift.api.model.DeploymentConfig
+import io.fabric8.openshift.api.model.Project
 import io.fabric8.openshift.client.DefaultOpenShiftClient
-import no.skatteetaten.aurora.model.ProjectInfo
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import java.time.Instant
 
 
 @Service
 class CrawlService {
 
+    // Makes logger availible
     val logger: Logger = LoggerFactory.getLogger(CrawlService::class.java)
 
-    // Test method
-    fun findAllProjects(): List<String> {
-        return DefaultOpenShiftClient().projects().list().items
-                .filter { it.metadata.labels != null }
-                .filter { it.metadata.labels.containsKey("removeAfter") }
-                //.filter { it.metadata.namespace }
-                .map {
-                    val epoch = System.currentTimeMillis() / 1000
-                    println(epoch)
-                    it.metadata.labels.get("removeAfter") ?: ""
-                }
+
+    fun test() {
+        val now: Instant = Instant.now()
+        logger.info("time: $now")
+        val epoch = System.currentTimeMillis()
+        logger.info("epoch: $epoch")
+        val instant = Instant.ofEpochMilli(epoch)
+        logger.info("back to time: $instant")
     }
 
 
-    fun listTtl(): List<ProjectInfo> {
-        return DefaultOpenShiftClient().projects().list().items
-                .filter { it.metadata.labels != null }
-                .filter { it.metadata.labels.containsKey("removeAfter") }
-                .map {
-                    val removeAfter = it.metadata.labels.get("removeAfter")?.toLong() ?: 0
-                    ProjectInfo( // Creates objects that contain Projectname and ttl time
-                            it.metadata.name,
-                            removeAfter
-                    )
-                }.filter { // Only includes projects that have expired
-                    val epoch = System.currentTimeMillis() / 1000
-                    it.removeAfter < epoch
-                }
+    fun HasMetadata.removeAfter(): Instant? {
+        val labels = this.metadata.labels ?: emptyMap() //
+        return labels["removeAfter"]?.let {
+            Instant.ofEpochSecond(it.toLong())
+        }
     }
+
+    private fun <T : HasMetadata> List<T>.isAfter(now: Instant): List<T> {
+        return this.filter { it.removeAfter()?.isAfter(now) ?: false }
+    }
+
+    //
+    fun findExpiredDc(): List<DeploymentConfig> {
+        logger.info("findExpiredDc() invoked!")
+        val dcs = DefaultOpenShiftClient().deploymentConfigs()
+                .list().items.isAfter(Instant.now())
+        return dcs
+    }
+
+    fun findExpiredProjectInfo(): List<Project> = DefaultOpenShiftClient().projects()
+            .list().items.isAfter(Instant.now())
+
 
     @Scheduled(fixedRate = 300_000, initialDelay = 2_000)
-    fun testKim() {
+    fun deleteProjects() {
         logger.info("Searching for projects to gorge")
+        val client = DefaultOpenShiftClient()
 
-        val namespacesToRemove = listTtl()
 
-                //.filter { it.name != "paas-kim-dev" } // does not delete itself
+        findExpiredProjectInfo()
                 .forEach {
-                    val removeAfterDate = java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(java.util.Date(it.removeAfter * 1000)) // converts epoch to date
-                    logger.info("Found project to devour: ${it.name}. time-to-live expired $removeAfterDate (dd/MM/yyyy HH:mm:ss)")
-                    DefaultOpenShiftClient().namespaces().withName(it.name).delete() // deletes namespace
-                    logger.info("Project ${it.name} gobbled, tastes like chicken!")
+                    logger.info("Found project to devour: ${it.metadata.name}. time-to-live expired ${it.removeAfter().toString()}")
+                    client.projects().withName(it.metadata.name).delete() // deletes namespace
+                    logger.info("Project ${it.metadata.name} gobbled, tastes like chicken!")
                 }
         logger.info("No more projects to feed on, will try again in a while")
 
     }
 
+    @Scheduled(fixedRate = 300_000, initialDelay = 2_000)
+    fun testKim() {
+        logger.info("Searching for applications to gorge")
+
+        val client = DefaultOpenShiftClient()
+        findExpiredDc()
+                .forEach {
+                    logger.info("Found app to devour: ${it.metadata.name}. time-to-live expired ${it.removeAfter().toString()}")
+                    client.deploymentConfigs()
+                            .inNamespace(it.metadata.namespace)
+                            .withLabel("app", it.metadata.name)
+                            .delete()
+
+                    client.services()
+                            .inNamespace(it.metadata.namespace)
+                            .withLabel("app", it.metadata.name)
+                            .delete()
+
+                    client.buildConfigs()
+                            .inNamespace(it.metadata.namespace)
+                            .withLabel("app", it.metadata.name)
+                            .delete()
+
+                    client.configMaps()
+                            .inNamespace(it.metadata.namespace)
+                            .withLabel("app", it.metadata.name)
+                            .delete()
+
+                    client.secrets()
+                            .inNamespace(it.metadata.namespace)
+                            .withLabel("app", it.metadata.name)
+                            .delete()
+
+                    client.imageStreams()
+                            .inNamespace(it.metadata.namespace)
+                            .withLabel("app", it.metadata.name)
+                            .delete()
+
+                    client.routes()
+                            .inNamespace(it.metadata.namespace)
+                            .withLabel("app", it.metadata.name)
+                            .delete()
+                }
+        logger.info("No more applications to feed on, will try again in a while")
+
+    }
 }
