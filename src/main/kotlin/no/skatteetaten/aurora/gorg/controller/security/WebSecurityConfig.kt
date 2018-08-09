3040b6d0d56f57318a492550f14e8d015f6a9c2c
Bjarte Stien Karlsen <bjarte.karlsen@skatteetate.no>
2018-08-09 14:43:36 +0200

    unused import

--- src/main/kotlin/no/skatteetaten/aurora/gorg/controller/security/WebSecurityConfig.kt
+++ src/main/kotlin/no/skatteetaten/aurora/gorg/controller/security/WebSecurityConfig.kt
@@ -1,6 +1,5 @@
 package no.skatteetaten.aurora.gorg.controller.security
 
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
