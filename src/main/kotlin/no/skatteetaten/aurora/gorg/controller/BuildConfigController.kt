94e8baad8e0826326ebe97a205e4c4bbb026d6b0
Erlend Haugsnes <erlend.haugsnes@skatteetaten.no>
2018-10-23 10:16:04 +0200

    Refactor build to buildConfig. Fixed delete for buildConfig. added dryrun property

---
+++ src/main/kotlin/no/skatteetaten/aurora/gorg/controller/BuildConfigController.kt
@@ -0,0 +1,27 @@
+package no.skatteetaten.aurora.gorg.controller
+
+import no.skatteetaten.aurora.gorg.service.BuildConfigService
+import no.skatteetaten.aurora.gorg.service.TemporaryBuildConfig
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+
+@RestController
+@RequestMapping("/api/buildConfigs")
+class BuildConfigController(val buildConfigService: BuildConfigService){
+
+    @DeleteMapping
+    fun deleteProjects() {
+        buildConfigService.findTemporaryBuildConfigs(Instant.now())
+            .filter { it.ttl.isNegative }
+            .forEach { buildConfigService.deleteBuildConfig(it) }
+    }
+
+    @GetMapping
+    fun list(): List<TemporaryBuildConfig> {
+        return buildConfigService.findTemporaryBuildConfigs(Instant.now())
+    }
+
+}
