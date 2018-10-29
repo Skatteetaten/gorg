114fa2e90fb4000eab7c66084a18566a0629575c
Erlend Haugsnes <erlend.haugsnes@skatteetaten.no>
2018-10-29 09:45:40 +0100

    Added more tests to increase coverage.

--- src/test/kotlin/no/skatteetaten/aurora/gorg/service/DeleteServiceTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/gorg/service/DeleteServiceTest.kt
@@ -6,6 +6,7 @@ import assertk.assertions.isFalse
 import assertk.assertions.isTrue
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry
 import no.skatteetaten.aurora.gorg.ApplicationDeploymentBuilder
+import no.skatteetaten.aurora.gorg.BuildConfigDataBuilder
 import no.skatteetaten.aurora.gorg.ProjectDataBuilder
 import no.skatteetaten.aurora.gorg.extensions.execute
 import no.skatteetaten.aurora.gorg.extensions.toResource
@@ -26,7 +27,7 @@ class DeleteServiceTest : AbstractOpenShiftServerTest() {
         val deleteService = DeleteService(openShiftServer.openshiftClient, meterRegsitry, true)
         val deleted = deleteService.deleteProject(project.toResource(Instant.now()))
         assert(deleted).isTrue()
-        assert(meterRegsitry.find(METRICS_DELETED_RESOURCES).counter()?.count()).isEqualTo(1.0)
+        assert(meterRegsitry.find(METRICS_DELETED_RESOURCES).tag("status", "deleted").counter()?.count()).isEqualTo(1.0)
     }
 
     @Test
@@ -36,6 +37,31 @@ class DeleteServiceTest : AbstractOpenShiftServerTest() {
             ProjectResource(
                 name = "non-existing-name",
                 ttl = Duration.ZERO,
+                removalTime = Instant.now()
+            )
+        )
+        assert(deleted).isFalse()
+    }
+
+    @Test
+    fun `Delete existing buildConfig`() {
+        val buildConfig = BuildConfigDataBuilder().build()
+        openShiftServer.openshiftClient.inNamespace("namespace").buildConfigs().create(buildConfig)
+
+        val deleteService = DeleteService(openShiftServer.openshiftClient, meterRegsitry, true)
+        val deleted = deleteService.deleteBuildConfig(buildConfig.toResource(Instant.now()))
+        assert(deleted).isTrue()
+        assert(meterRegsitry.find(METRICS_DELETED_RESOURCES).tag("status", "deleted").counter()?.count()).isEqualTo(1.0)
+    }
+
+    @Test
+    fun `Return not deleted for non-existing buildConfig`() {
+        val deleteService = DeleteService(openShiftServer.openshiftClient, meterRegsitry, true)
+        val deleted = deleteService.deleteBuildConfig(
+            BuildConfigResource(
+                name = "non-existing-name",
+                ttl = Duration.ZERO,
+                namespace = "namespace",
                 removalTime = Instant.now()
             )
         )
@@ -53,6 +79,37 @@ class DeleteServiceTest : AbstractOpenShiftServerTest() {
         }
         assert(request.method).isEqualTo("DELETE")
         assert(request.path).isEqualTo("/apis/skatteetaten.no/v1/namespaces/${ad.metadata.namespace}/applicationdeployments/${ad.metadata.name}")
-        assert(meterRegsitry.find(METRICS_DELETED_RESOURCES).counter()?.count()).isEqualTo(1.0)
+        assert(meterRegsitry.find(METRICS_DELETED_RESOURCES).tag("status", "deleted").counter()?.count()).isEqualTo(1.0)
+    }
+
+    @Test
+    fun `return not deleted for non-existing applicationDeployments`() {
+        val deleteService = DeleteService(mockClient, meterRegsitry, true)
+        mockServer.execute(
+            404, false
+        ) {
+            assert(
+                deleteService.deleteApplicationDeployment(
+                    ApplicationDeploymentResource(
+                        name = "non-existing-name",
+                        ttl = Duration.ZERO,
+                        namespace = "namespace",
+                        removalTime = Instant.now()
+                    )
+                )
+            ).isFalse()
+        }
+        assert(meterRegsitry.find(METRICS_DELETED_RESOURCES).tag("status", "error").counter()?.count()).isEqualTo(1.0)
+    }
+
+    @Test
+    fun `return skipped if deleteResource is false`() {
+        val buildConfig = BuildConfigDataBuilder().build()
+        openShiftServer.openshiftClient.inNamespace("namespace").buildConfigs().create(buildConfig)
+
+        val deleteService = DeleteService(openShiftServer.openshiftClient, meterRegsitry, false)
+        val deleted = deleteService.deleteBuildConfig(buildConfig.toResource(Instant.now()))
+        assert(deleted).isFalse()
+        assert(meterRegsitry.find(METRICS_DELETED_RESOURCES).tag("status", "skipped").counter()?.count()).isEqualTo(1.0)
     }
 }
