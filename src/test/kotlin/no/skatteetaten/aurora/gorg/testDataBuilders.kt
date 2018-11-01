a5a197db56a3824fdc4961e8ec8d6ed498e121f3
Jarle Hansen <jarle@jarlehansen.net>
2018-11-01 13:18:39 +0100

    added tests for CrawlController (#10)

--- src/test/kotlin/no/skatteetaten/aurora/gorg/testDataBuilders.kt
+++ src/test/kotlin/no/skatteetaten/aurora/gorg/testDataBuilders.kt
@@ -9,61 +9,82 @@ import io.fabric8.openshift.api.model.BuildConfig
 import io.fabric8.openshift.api.model.Project
 import no.skatteetaten.aurora.gorg.extensions.REMOVE_AFTER_LABEL
 import no.skatteetaten.aurora.gorg.model.ApplicationDeployment
+import no.skatteetaten.aurora.gorg.service.ApplicationDeploymentResource
+import no.skatteetaten.aurora.gorg.service.BuildConfigResource
+import no.skatteetaten.aurora.gorg.service.ProjectResource
+import java.time.Duration
 import java.time.Instant
 
 data class BuildConfigDataBuilder(
-    val bcNamespace: String = "namespace",
-    val bcKind: String = "Deployment",
-    val bcName: String = "name",
-    val bcTtl: Instant = Instant.now().plusSeconds(60)
+        val bcNamespace: String = "namespace",
+        val bcKind: String = "Deployment",
+        val bcName: String = "name",
+        val bcTtl: Instant = Instant.now().plusSeconds(60)
 ) {
 
     fun build(): BuildConfig =
-        newBuildConfig {
-            kind = bcKind
-            metadata = newObjectMeta {
-                name = bcName
-                namespace = bcNamespace
-                labels = mapOf(REMOVE_AFTER_LABEL to bcTtl.epochSecond.toString())
+            newBuildConfig {
+                kind = bcKind
+                metadata = newObjectMeta {
+                    name = bcName
+                    namespace = bcNamespace
+                    labels = mapOf(REMOVE_AFTER_LABEL to bcTtl.epochSecond.toString())
+                }
             }
-        }
 }
 
 data class ApplicationDeploymentBuilder(
-    val adNamespace: String = "namespace",
-    val adKind: String = "ApplicationDeployment",
-    val adName: String = "name",
-    val adTtl: Instant = Instant.now().plusSeconds(60)
+        val adNamespace: String = "namespace",
+        val adKind: String = "ApplicationDeployment",
+        val adName: String = "name",
+        val adTtl: Instant = Instant.now().plusSeconds(60)
 ) {
 
     fun build(): ApplicationDeployment {
         return ApplicationDeployment(
-            kind = adKind,
-            metadata = newObjectMeta {
-                name = adName
-                namespace = adNamespace
-                labels = mapOf(REMOVE_AFTER_LABEL to adTtl.epochSecond.toString())
-            }
+                kind = adKind,
+                metadata = newObjectMeta {
+                    name = adName
+                    namespace = adNamespace
+                    labels = mapOf(REMOVE_AFTER_LABEL to adTtl.epochSecond.toString())
+                }
         )
     }
 }
 
 data class ProjectDataBuilder(
-    val pName: String = "name",
-    val pPhase: String = "phase",
-    val pTtl: Instant = Instant.now().plusSeconds(60)
+        val pName: String = "name",
+        val pPhase: String = "phase",
+        val pTtl: Instant = Instant.now().plusSeconds(60)
 ) {
 
     fun build(): Project =
-        newProject {
-            status {
-                phase = pPhase
+            newProject {
+                status {
+                    phase = pPhase
+                }
+                metadata {
+                    name = pName
+                    labels = mapOf(
+                            REMOVE_AFTER_LABEL to pTtl.epochSecond.toString()
+                    )
+                }
             }
-            metadata {
-                name = pName
-                labels = mapOf(
-                    REMOVE_AFTER_LABEL to pTtl.epochSecond.toString()
-                )
-            }
-        }
+}
+
+data class ProjectResourceBuilder(val ttl: Duration = Duration.ofSeconds(100)) {
+    fun build() =
+            ProjectResource("name", ttl, Instant.now().plusSeconds(100))
+}
+
+data class BuildConfigResourceBuilder(val ttl: Duration = Duration.ofSeconds(100)) {
+
+    fun build() =
+            BuildConfigResource("name", "namespace", ttl, Instant.now().plusSeconds(100))
+}
+
+data class ApplicationDeploymentResourceBuilder(val ttl: Duration = Duration.ofSeconds(100)) {
+
+    fun build() =
+            ApplicationDeploymentResource("name", "namespace", ttl, Instant.now().plusSeconds(100))
 }
