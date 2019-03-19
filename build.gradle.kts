e3031380be198c3534dacff9d04812bff62c34c0
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2019-03-19 09:05:26 +0100

    upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.21"
     id("org.jetbrains.kotlin.plugin.spring") version "1.3.21"
-    id("org.jlleitschuh.gradle.ktlint") version "7.1.0"
+    id("org.jlleitschuh.gradle.ktlint") version "7.2.1"
 
     id("org.springframework.boot") version "2.1.3.RELEASE"
     id("org.asciidoctor.convert") version "1.6.0"
@@ -10,21 +10,21 @@ plugins {
     id("com.github.ben-manes.versions") version "0.21.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.9"
 
-    id("no.skatteetaten.gradle.aurora") version "2.0.2"
+    id("no.skatteetaten.gradle.aurora") version "2.2.0"
 
 }
 
 dependencies {
     implementation("io.fabric8:openshift-client:4.1.3")
     testImplementation("com.fkorotkov:kubernetes-dsl:2.0.1")
-    testImplementation("com.squareup.okhttp3:mockwebserver:3.13.1")
+    testImplementation("com.squareup.okhttp3:mockwebserver:3.14.0")
 
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    testImplementation("io.mockk:mockk:1.9.1")
+    testImplementation("io.mockk:mockk:1.9.2")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.13")
     testImplementation("com.nhaarman:mockito-kotlin:1.6.0")
 }
