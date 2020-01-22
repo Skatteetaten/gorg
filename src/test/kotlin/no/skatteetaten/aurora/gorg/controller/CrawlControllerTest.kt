81b9a51a707a5daca23718824732f134aded42ed
Erlend Haugsnes <Erlend.Haugsnes@skatteetaten.no>
2020-01-22 12:35:45 +0100

    ktlint

--- src/test/kotlin/no/skatteetaten/aurora/gorg/controller/CrawlControllerTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/gorg/controller/CrawlControllerTest.kt
@@ -20,13 +20,9 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.test.context.support.WithMockUser
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
